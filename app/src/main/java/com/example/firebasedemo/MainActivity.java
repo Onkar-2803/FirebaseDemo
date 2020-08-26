f771d32951932643e3e24b8bfe97f1c63fb14397
Onkar-2803 <jr.onkarlitake@gmail.com>
2020-08-26 13:35:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.firebasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button logout;
+    private EditText edit;
+    private Button add;
+    private ListView listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
+        listview = findViewById(R.id.listview);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_name = edit.getText().toString();
+                if(txt_name.isEmpty()){
+                    Toast.makeText(MainActivity.this, "No name entered", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    FirebaseDatabase.getInstance().getReference().child("ProgrammingKnowledge").push().child("Name").setValue(txt_name);
+                }
+            }
+        });
+
+        ArrayList<String> list = new ArrayList<>();
+        //ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.this_item);
+    }
+}
